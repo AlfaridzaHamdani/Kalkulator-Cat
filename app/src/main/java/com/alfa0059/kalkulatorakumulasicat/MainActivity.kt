6f453a6d2d8d32d8d2ece5a4f56e508b99c745d2
Alfaridza Hamdani <alfaridzah@gmail.com>
2025-04-10 15:04:30 +0700

    Add new project

---
+++ app/src/main/java/com/alfa0059/kalkulatorakumulasicat/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.alfa0059.kalkulatorakumulasicat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alfa0059.kalkulatorakumulasicat.ui.theme.KalkulatorAkumulasiCatTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KalkulatorAkumulasiCatTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KalkulatorAkumulasiCatTheme {
+        Greeting("Android")
+    }
+}
